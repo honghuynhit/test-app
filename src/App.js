ae3670cc31b1382be0b18d2f939cf392002085bb
huynhdh <huynhdh@minerva.vn>
2020-07-21 11:42:04 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+// Just a data fetching function
+const fetchURL = "https://ga-django-backend.herokuapp.com/users/";
+const getItems = () => fetch(fetchURL).then(res => res.json());
+
+function List({ items, fallback }) {
+  if (!items || items.length === 0) {
+    return fallback;
+  } else {
+    return items.map(item => {
+      return <div key={item.username}>{item.username}</div>;
+    });
+  }
+}
+
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getItems().then(data => setItems(data.results));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <List items={items} fallback={"Loading..."} />
     </div>
   );
 }
